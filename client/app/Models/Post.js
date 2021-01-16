7ed3420bb472a980d398aa5efdd53e10f4d47ec8
pburkett <pburkett636@gmail.com>
2021-01-15 21:53:41 -0700

    group changes

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -14,20 +14,42 @@ export default class Post {
     }
 
     get Template() {
-        let template = `
-    <div class="card post">
-      <div class="card-header text-right">
-      ${this.voteCount}
-        <i class="far fa-arrow-alt-circle-up fa-3x one icon" onclick="app.postsController.vote(true, '${this.id}')"></i> <i class="far fa-arrow-alt-circle-down fa-3x two icon" onclick="app.postsController.vote(false, '${this.id}')"></i>
-      </div>
-      <img class="card-img-top" src="${this.URL}" alt="">
-      <button class="btn btn-success" onclick="app.commentsController.addComment('${this.insult}', '${this.id}')">${this.insult}</button>
-    <div class="card-body" id="comments"></div>
-    </div>
-    `
+        let template = `<div class="card post">
+            <div class="card-header text-right header">
+              <i class="far fa-arrow-alt-circle-up fa-3x one icon" onclick=""></i>  
+              <h3 class="d-inline text-white mx-2">${this.voteCount}</h3> 
+              <i class="far fa-arrow-alt-circle-down fa-3x two icon" onclick=""></i>
+            </div>
+            <div class="row">
+            <div class="col"></div>
+            <div class = "col-12">
+            <img class="card-img-top pic pb-3" src="${this.URL}" alt="">
+            </div>
+            <div class = "col"></div>
+            </div>
+          <div class="card-body" id="comments">
+          <button id="mean" type="button" class="btn btn-primary btn-lg btn-block">${this.compliment}</button>
+          <div class="text-center or"><b> -OR- </b></div>
+      <button type="button" class="btn btn-primary btn-lg btn-block">${this.insult}</button>
+          </div>
+          </div>
+          `
+
 
         this.comments.forEach(c => template += c.Template)
 
         return template
     }
 }
+
+
+// <div class="card post">
+//       <div class="card-header text-right">
+//       ${this.voteCount}
+//         <i class="far fa-arrow-alt-circle-up fa-3x one icon" onclick="app.postsController.vote(true, '${this.id}')"></i> <i class="far fa-arrow-alt-circle-down fa-3x two icon" onclick="app.postsController.vote(false, '${this.id}')"></i>
+//       </div>
+//       <img class="card-img-top" src="${this.URL}" alt="">
+//       <button class="btn btn-success" onclick="app.commentsController.addComment('${this.insult}', '${this.id}')">${this.insult}</button>
+//     <div class="card-body" id="comments"></div>
+//     </div>
+//     `
