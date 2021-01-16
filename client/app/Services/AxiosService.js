01c3fab715b7709022c1447cbd0fabbe495655dd
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-01-15 23:49:55 -0700

    axios

--- client/app/Services/AxiosService.js
+++ client/app/Services/AxiosService.js
@@ -1,7 +1,7 @@
 const base = window.location.host.includes('localhost') ? '//localhost:3000/' : '/'
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://positivity-hackathon.herokuapp.com/api",
     timeout: 3000,
     withCredentials: true
 })
